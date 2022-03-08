82d46190d02e492395f18d1c1271c5a7b97d7f7b
David Freire Fernández <D.FreireFernandez@gsi.de>
2022-03-08 10:47:47 +0100

    adding method for particle

--- simtofat/fat.py
+++ simtofat/fat.py
@@ -31,25 +31,30 @@ def import_particles_from_lise(lisefile):
     return lise_data
 
 def get_energy_isomer(delta_f, moq_gs, charge, fref, gammat, harmonic):
-    moq_isomer=moq_gs*(-delta_f/harmonic*gammat**2/fref+1)
+    moq_isomer=moq_gs * ( -delta_f / harmonic * gammat**2 / fref + 1)
     energy_respect_gs = (moq_isomer - moq_gs) * charge
-    print(AMEData.to_mev(moq_gs*charge), AMEData.to_mev(moq_isomer*charge), fref)
     return AMEData.to_mev(energy_respect_gs)
 
 def get_delta_frecuency_isomer(energy_isomer_respect_gs, moq_gs, charge, fref, gammat):
-    return (fref*energy_isomer_respect_gs)/(charge*moq_gs*gammat**2)
+    #Introudce the energy in MeV
+    return (fref * energy_isomer_respect_gs) / (AMEData.to_mev(charge * moq_gs) * gammat**2)
+
+def get_frecuency_particle_ring(particle, f_ref, gammat, moq_ref):
+    #f_ref is the real revolution frec (harmonic) of the ref. particle
+    moq = particle.get_ionic_moq_in_u()
+    return (1 - 1 / gammat / gammat * (moq-moq_ref) / moq_ref) * f_ref
 
 def main():
-    charge=32
-    ref_iso='72Ge'
-    brho=6.9303
-    gammat=1.395
-    delta_f=6e3
-    imp=ImportData()
+    charge = 32
+    ref_iso = '72Ge'
+    brho = 6.9303
+    gammat = 1.395
+    delta_f = 6e3
+    imp = ImportData()
     imp.set_ref_ion(ref_iso, charge)
     imp._import('e.lpp')
     imp._calculate(brho, gammat, charge)
-    e_isomer=get_energy_isomer(delta_f, imp.moq[imp.ref_ion], charge, imp.frequence_rel, gammat, 209)
+    e_isomer = get_energy_isomer(delta_f, imp.moq[imp.ref_ion], charge, imp.frequence_rel, gammat, 209)
     print(e_isomer)
 
 if __name__ == '__main__':
